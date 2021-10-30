af4d86c40318aa56fc64b078813cfdb52b2e8061
wagabooga <64753034+wagabooga@users.noreply.github.com>
2021-10-29 22:32:30 -0700

    created initial form component with a few helper functions, as well as added to storybook

--- stories/index.js
+++ stories/index.js
@@ -17,7 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
 import Error from "components/Appointment/Error.js"
-
+import Form from "components/Appointment/Form.js"
 
 
 storiesOf("Button", module)
@@ -156,4 +156,6 @@ storiesOf("Appointment", module)
   ))
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleting"/>)
-  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+  .add("Edit", () => <Form student="Matthew" interviewer={interviewer.id} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Create", () => <Form interviewers={interviewers}  onSave={action("onSave")} onCancel={action("onCancel")} />);
