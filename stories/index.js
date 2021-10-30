b2525e01281974a09457064749f30d0b52c881a7
wagabooga <64753034+wagabooga@users.noreply.github.com>
2021-10-29 20:28:33 -0700

    created inital header and story

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,7 @@ import DayList from "components/DayList.js";
 import InterviewerListItem from "components/InterviewerListItem.js";
 import InterviewerList from "components/InterviewerList.js"
 import Appointment from "components/Appointment/index.js";
-
+import Header from "components/Appointment/Header.js"
 
 
 storiesOf("Button", module)
@@ -144,4 +144,6 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />);
+
