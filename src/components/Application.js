03a716f7315dbbfb53244de359390a984964d5e7
wagabooga <mattanthu@hotmail.com>
2021-11-02 21:17:43 -0700

    deleting appointments now works

--- src/components/Application.js
+++ src/components/Application.js
@@ -25,19 +25,57 @@ export default function Application(props) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      console.log(all[2].data)
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, [])
-  
+
   const dailyAppointments = getAppointmentsForDay(state, state.day)
   const dailyInterviewers = getInterviewersByDay(state, state.day)
-
   const appointments = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     return (
-      <Appointment key={appointment.id}  {...appointment}   interview={interview} interviewers={dailyInterviewers} />
+      <Appointment cancelInterview={cancelInterview} bookInterview={bookInterview} key={appointment.id}  {...appointment} interview={interview} interviewers={dailyInterviewers} />
     );
   });
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
+      setState({
+        ...state,
+        appointments
+      });
+    })
+  }
+
+  function cancelInterview(id) {
+    console.log("ID AND INTERVIEW", id)
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState({
+        ...state,
+        appointments
+      });
+    })
+  }
+
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +86,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={state.days} value={state.day} onChange={(day) => {setDay(day)}} />
+          <DayList days={state.days} value={state.day} onChange={(day) => { setDay(day) }} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,7 +6,13 @@ import useVisualMode from "hooks/useVisualMode.js";
 
 import "components/Appointment/styles.scss";
 import Form from "./Form.js";
+<<<<<<< Updated upstream
 
+=======
+import Status from "./Status.js";
+import cancelInterview from "../Application"
+import Confirm from "./Confirm.js";
+>>>>>>> Stashed changes
 // helper function
 const formatAppointment = function (time) {
   if (!time) {
@@ -21,30 +27,64 @@ const formatAppointment = function (time) {
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
-
+const SAVING = "SAVING";
+const CONFIRM = "CONFIRM"
+const DELETING = "DELETING"
 export default function Appointment(props) {
+  const bookInterview = props.bookInterview
+  const cancelInterview = props.cancelInterview
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING)
+    bookInterview(props.id, interview)
+      .then(() => { transition(SHOW) })
+  }
+
+  function transitionToConfirm() {
+    transition(CONFIRM)
+  }
 
 
+  function confirmDelete(id) {
+    transition(DELETING)
+    cancelInterview(id).then(() => { transition(EMPTY) })
+  }
+  console.log("mode:", mode)
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() =>  transition(CREATE)} />}
-      {mode === SHOW && (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && props.interview && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={transitionToConfirm}
         />
       )}
       {mode === CREATE && (
         <Form onCancel={back}
-        interviewers={props.interviewers}
+          onSave={save}
+          interviewers={props.interviewers}
         />
       )}
+      {mode === SAVING && (
+        <Status message={"Saving"} />
+      )}
+      {mode === DELETING && (
+        <Status message={"Deleting"} />
+      )}
+      {mode === CONFIRM && (
+        <Confirm onCancel={back} onConfirm={() => { confirmDelete(props.id) }} message={"Are you sure you would like to delete?"} />
+      )}
+
     </article>
   );
 }
