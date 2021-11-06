24a4680cd1a691bfc352d69567f43ae0d6af6ea1
wagabooga <mattanthu@hotmail.com>
2021-11-05 23:51:47 -0700

    fixed interviewer?

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,8 +21,8 @@ export default function Form(props) {
   }
 
   function validate() {
-    if (student === "") {
-      setError("Student name cannot be blank");
+    if (student === "" || interviewer === "") {
+      setError("Student name OR Interviewer cannot be blank");
       return;
     }
   
